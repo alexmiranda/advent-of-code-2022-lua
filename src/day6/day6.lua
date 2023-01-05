e660f334a57d42d342e7bff04c111a39f330498d
Alex Miranda <alexmirandamoraes@gmail.com>
2023-01-05 19:43:10 +0100

    Day 6 - part 2

--- src/day6/day6.lua
+++ src/day6/day6.lua
@@ -1,23 +1,32 @@
 local day6 = {}
 
 local function unique_chars(str)
-    local set = {}
+    local seen = {}
     for i = 1, #str do
         local letter = str:sub(i, i)
         -- found a repeated letter, aborting...
-        if set[letter] then return false end
-        set[letter] = true
+        if seen[letter] then return false end
+        seen[letter] = true
     end
     return true
 end
 
-function day6.start_of_packet(text)
-    for i = 4, #text do
-        local str = text:sub(i - 4, i)
+local function find_unique_sequence(text, length)
+    for i = length, #text do
+        local str = text:sub(i - length + 1, i)
+        assert(#str == length)
         if unique_chars(str) then
-            return i - 1
+            return i
         end
     end
+end
+
+function day6.start_of_packet(text)
+    return find_unique_sequence(text, 4)
+end
+
+function day6.start_of_message(text)
+    return find_unique_sequence(text, 14)
 end
 
 return day6
--- src/day6/day6_spec.lua
+++ src/day6/day6_spec.lua
@@ -22,7 +22,24 @@ describe("day6", function()
         end
     end)
 
-    it("puzzle input - part 1", function ()
+    it("puzzle input - part 1", function()
         assert.are_equals(1848, day6.start_of_packet(puzzle_input))
+    end)
+
+    it("example - part 2", function()
+        local tests = {
+            { text = "mjqjpqmgbljsphdztnvjfqwrcgsmlb", start_of_message = 19 },
+            { text = "bvwbjplbgvbhsrlpgdmjqwftvncz", start_of_message = 23 },
+            { text = "nppdvjthqldpwncqszvftbrmjlhg", start_of_message = 23 },
+            { text = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", start_of_message = 29 },
+            { text = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", start_of_message = 26 },
+        }
+        for _, t in ipairs(tests) do
+            assert.are.equals(t.start_of_message, day6.start_of_message(t.text))
+        end
+    end)
+
+    it("puzzle input - part 2", function()
+        assert.are_equals(2308, day6.start_of_message(puzzle_input))
     end)
 end)
