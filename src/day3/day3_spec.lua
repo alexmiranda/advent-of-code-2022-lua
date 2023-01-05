3e92e18e0bfbb6af55dc90b993748477eadb574d
Alex Miranda <alexmirandamoraes@gmail.com>
2023-01-05 01:08:11 +0100

    Day 3 - part 2

--- src/day3/day3_spec.lua
+++ src/day3/day3_spec.lua
@@ -1,6 +1,9 @@
 ---@diagnostic disable: undefined-global
 
-local EXAMPLE = [[
+describe("day3", function()
+    local day3 = require("day3")
+    local puzzle_input = nil
+    local EXAMPLE = [[
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
@@ -9,16 +12,24 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 ]]
 
-describe("day3", function()
-    local day3 = require("day3")
+    setup(function ()
+        local f = assert(io.open("src/day3/input", "r"))
+        puzzle_input = f:read("*all")
+    end)
 
     it("example - part 1", function()
         assert.are.equal(157, day3.sum_of_priorities(EXAMPLE))
     end)
 
     it("puzzle input - part 1", function()
-        local f = assert(io.open("src/day3/input", "r"))
-        local content = f:read("*all")
-        assert.are.equal(7848, day3.sum_of_priorities(content))
+        assert.are.equal(7848, day3.sum_of_priorities(puzzle_input))
+    end)
+
+    it("example - part 2", function()
+        assert.are.equal(70, day3.sum_of_group_priorities(EXAMPLE))
+    end)
+
+    it("puzzle input - part 2", function()
+        assert.are.equal(2616, day3.sum_of_group_priorities(puzzle_input))
     end)
 end)
