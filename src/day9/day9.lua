aa35cf7a7c43c1fca1946793244eb90a27b67859
Alex Miranda <alexmirandamoraes@gmail.com>
2023-01-06 04:01:34 +0100

    Day 9 - part 2

--- src/day9/day9.lua
+++ src/day9/day9.lua
@@ -1,13 +1,14 @@
 local day9 = {}
 
---luacheck: ignore inspect
+-- luacheck: ignore inspect
 local inspect = require("inspect")
+
+local hash = function(pos)
+    return tostring(pos.y) .. "," .. tostring(pos.x)
+end
 
 function day9.count_visited(input)
     local head, tail = { x = 0, y = 0 }, { x = 0, y = 0 }
-    local hash = function(pos)
-        return tostring(pos.y) .. "," .. tostring(pos.x)
-    end
     local seen = { [hash(tail)] = true }
     local counter = 1
     for dir, n in input:gmatch("([LURD]) (%d+)") do
@@ -35,6 +36,54 @@ function day9.count_visited(input)
             end
         end
         -- print(inspect(seen))
+    end
+    return counter
+end
+
+function day9.count_visited_long_rope(input, length)
+    local start = { x = 0, y = 0 }
+    local rope = {}
+    for _ = 1, length do
+        table.insert(rope, { x = 0, y = 0 })
+    end
+    local seen = { [hash(start)] = true }
+    local counter = 1
+    -- print(inspect(rope))
+    for dir, n in input:gmatch("([LURD]) (%d+)") do
+        -- print(dir, n)
+        for _ = 1, tonumber(n) do
+            local head = rope[1]
+
+            -- just move the head first
+            if dir == "L" then
+                head.x = head.x - 1
+            elseif dir == "U" then
+                head.y = head.y - 1
+            elseif dir == "R" then
+                head.x = head.x + 1
+            elseif dir == "D" then
+                head.y = head.y + 1
+            end
+
+            -- move the rest of the rope accordingly
+            for i = 2, length do
+                local next = rope[i]
+                local delta_x, delta_y = head.x - next.x, head.y - next.y
+                if math.abs(delta_x) > 1 or math.abs(delta_y) > 1 then
+                    if delta_x ~= 0 then next.x = next.x + (delta_x / math.abs(delta_x)) end
+                    if delta_y ~= 0 then next.y = next.y + (delta_y / math.abs(delta_y)) end
+                end
+                head = next
+            end
+
+            -- check if the tail visited this position before
+            local h = hash(rope[length])
+            -- print(inspect(rope[length]))
+            if not seen[h] then
+                seen[h] = true
+                counter = counter + 1
+            end
+        end
     end
     return counter
 end
--- src/day9/day9_spec.lua
+++ src/day9/day9_spec.lua
@@ -2,6 +2,7 @@
 
 describe("day9", function()
     local day9 = require("day9")
+    local puzzle_input = nil
 
     local EXAMPLE = [[
 R 4
@@ -14,15 +15,41 @@ L 5
 R 2
 ]]
 
+    local LARGER_EXAMPLE = [[
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+]]
+
+    setup(function()
+        local f = assert(io.open("src/day9/input", "r"))
+        puzzle_input = f:read("*all")
+    end)
+
     it("example - part 1", function()
         local answer = day9.count_visited(EXAMPLE)
         assert.are.equals(13, answer)
+        assert.are.equals(answer, day9.count_visited_long_rope(EXAMPLE, 2))
     end)
 
     it("puzzle input - part 1", function()
-        local f = assert(io.open("src/day9/input", "r"))
-        local puzzle_input = f:read("*all")
         local answer = day9.count_visited(puzzle_input)
         assert.are.equals(6067, answer)
+        assert.are.equals(answer, day9.count_visited_long_rope(puzzle_input, 2))
+    end)
+
+    it("example - part 2", function()
+        local answer = day9.count_visited_long_rope(LARGER_EXAMPLE, 10)
+        assert.are.equals(36, answer)
+    end)
+
+    it("puzzle input - part 2", function()
+        local answer = day9.count_visited_long_rope(puzzle_input, 10)
+        assert.are.equals(2471, answer)
     end)
 end)
